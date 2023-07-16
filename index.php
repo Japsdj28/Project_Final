8ceec368422a90db0e20088db77c8b08effad407
Japsdj28 <133623500+Japsdj28@users.noreply.github.com>
2023-07-17 02:31:11 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,120 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-include("connection/connect.php");  
-error_reporting(0);  
-session_start(); 
+include("../connection/connect.php");
+error_reporting(0);
+session_start();
+if(isset($_POST['submit']))
+{
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	if(!empty($_POST["submit"])) 
+     {
+	$loginquery ="SELECT * FROM admin WHERE username='$username' && password='".md5($password)."'";
+	$result=mysqli_query($db, $loginquery);
+	$row=mysqli_fetch_array($result);
+	
+	                        if(is_array($row))
+								{
+                                    	$_SESSION["adm_id"] = $row['adm_id'];
+										header("refresh:1;url=dashboard.php");
+	                            } 
+							else
+							    {
+										echo "<script>alert('Invalid Username or Password!');</script>"; 
+                                }
+	 }
+	
+}
 
 ?>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Admin Login</title>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="#">
-    <title>Home || Online Gadget Shop</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/animsition.min.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-</head>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+                    <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
+                    <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
+                    <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+
+                    <link rel="stylesheet" href="css/login.css">
 
 
-<body class="home">
+                </head>
 
-    <header id="header" class="header-scroll top-header headrom">
-        <nav class="navbar navbar-dark">
-            <div class="container">
-            <form class="form-inline my-2 my-lg-0" action="products.php" method="get">
-                        <input class="form-control mr-sm-2" type="text" placeholder="Search" name="search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
-                <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="" alt="" width="18%"> </a>
-                <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
-                    <ul class="nav navbar-nav">
-                        <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                <body>
+
+
+                    <div class="container">
+                        <div class="info">
+                            <h1>Admin Panel </h1>
+                        </div>
+                    </div>
+                    <div class="form">
+                        <div class="thumbnail"><img src="images/manager.png" /></div>
+                        <span style="color:red;"><?php echo $message; ?></span>
+                        <span style="color:green;"><?php echo $success; ?></span>
+                        <form class="login-form" action="index.php" method="post">
+                            <input type="text" placeholder="Username" name="username" />
+                            <input type="password" placeholder="Password" name="password" />
+                            <input type="submit" name="submit" value="Login" />
+
                         </form>
-                        <?php
-						if(empty($_SESSION["user_id"])) // if user is not login
-							{
-								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
-							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
-							}
-						else
-							{
 
-									
-									echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
-									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
-							}
 
-						?>
+                    </div>
+                    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+                    <script src='js/index.js'></script>
+                </body>
 
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
-
-    </header>
-
-    <section class="hero bg-image" data-image-src="images/img/bgimage.jpg">
-        <div class="hero-inner">
-            <div class="container text-center hero-text font-white">
-                <h1>Gizmos Gadgets Shop</h1>
-
-                <div class="banner-form">
-                    <form class="form-inline">
-
-                    </form>
-                </div>
-
-            </div>
-        </div>
-
-    </section>
-
-    <section class="popular">
-        <div class="container">
-            <div class="title text-xs-center m-b-30">
-                <h2>Recently Added Products</h2>
-                <p class="lead">Check out this newly added products!</p>
-            </div>
-            <div class="row">
-
-                <?php 					
-						$query_res= mysqli_query($db,"select * from dishes LIMIT 6"); 
-                                while($r=mysqli_fetch_array($query_res))
-                                {
-                                        
-                                    echo '  <div class="col-xs-12 col-sm-6 col-md-4 food-item">
-                                            <div class="food-item-wrap">
-                                                <div class="figure-wrap bg-image" data-image-src="admin/Res_img/dishes/'.$r['img'].'"></div>
-                                                <div class="content">
-                                                    <h5><a href="products.php?res_id='.$r['rs_id'].'">'.$r['title'].'</a></h5>
-                                                    <div class="product-name">'.$r['slogan'].'</div>
-                                                    <div class="price-btn-block"> <span class="price">Php'.$r['price'].'</span> <a href="products.php?res_id='.$r['rs_id'].'" class="btn theme-btn-dash pull-right">Order Now</a> </div>
-                                                </div>
-                                                
-                                            </div>
-                                    </div>';                                      
-                                }	
-						?>
-            </div>
-        </div>
-    </section>
-
-    <script src="js/jquery.min.js"></script>
-    <script src="js/tether.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/animsition.min.js"></script>
-    <script src="js/bootstrap-slider.min.js"></script>
-    <script src="js/jquery.isotope.min.js"></script>
-    <script src="js/headroom.js"></script>
-    <script src="js/foodpicky.min.js"></script>
-</body>
-
-</html>
+                </html>
